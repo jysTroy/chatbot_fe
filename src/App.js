be9dca3ee212f1116c5caa5102f4b99c08b7965b
yesung <jys_troy@naver.com>
2025-07-09 18:09:47 +0900

    fe start

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          리액트 배우러 가볼까요오오오옹???????????
         </a>
       </header>
     </div>
